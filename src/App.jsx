f7aba3ee5646219d250fe456c217329c0e78e7e9
azahra01 <ogilvied97@gmail.com>
2022-12-11 19:52:56 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-function App() {
-  return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
-  )
+import React, { Component } from "react";
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <h1> Meet the {this.props.name}</h1>;
+  }
 }
-export default App
+
+function App() {
+  const name = "StarGazers";
+  return (
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="StarGazers Group" />
+          <Welcome name="Stars" />
+          <Welcome name="StarsGazers" />
+          <p>
+            Members of an <b>intergalactic alliance</b> paving the way for peace
+            and benevolence among all species. They are known for their
+            enthusiasm for science, for their love of fun, and their dedication
+            to education.
+          </p>
+          <button className="outline" onClick={() => alert("Hi There!")}>
+            Click me
+          </button>
+        </hgroup>
+      </article>
+    </div>
+  );
+}
+export default App;
